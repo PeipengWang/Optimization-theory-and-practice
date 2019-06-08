7d2063362cd17b45430ae7c63c5345f0203d7c27
PeipengWang <wangpeipeng123@163.com>
2019-06-08 13:35:35 +0800

    电流优化成功，相位还未曾优化

--- MSLL3.m
+++ MSLL3.m
@@ -1,12 +1,18 @@
 %%%计算最大旁瓣%%%
 function  MSLL3 = MSLL3(d,beta,NN,NL,seta0,f,L,maxE)
 seta = linspace(-pi,pi,NN);
-alfa = (2*pi*(f(1:L/2)*(2.^(0:L/2-1)'))/(2^(L/2)-1)); %解码
+%alfa = (2*pi*(f(1:L/2)*(2.^(0:L/2-1)'))/(2^(L/2)-1)); %解码
+alfa = 0;
 x = maxE/8*f(L/2+1:L)*(2.^(0:L/2-1)')/(2^(L/2)-1); %解码
+%I = [0.2035,0.2792,0.4089,0.5625,0.7147,0.8474,0.9466,1,1,0.9466,0.8474,...
+ %   0.7147,0.5625,0.4089,0.2792,0.2035];
 
-I1 = linspace(maxE-(NL/2)*x,maxE,NL/2);
-I2 = linspace(maxE,maxE-(NL/2*x),NL/2);
-I = [I1,I2]-x;
+for  i = 0:NL/2-1
+    I1(i+1) = (maxE*(f(i*L/16+1:(i+1)*L/16)*(2.^(0:(L/16-1))'))/(2^(L/16)-1));
+end
+I2 = fliplr(I1);
+I = [I1,I2];
+
 for m = 1:NN
     fai = beta*d*([0:NL-1]+1-(NL+1)/2)*cos(seta(m)-seta0)-[0:NL-1]*alfa;
     Fit(m) = abs(sum(I*exp(sqrt(-1)*(fai)')));
@@ -17,7 +23,7 @@ FdB = 20*log10(Fit/max(Fit));
 
 %那如果需要找到其中满足一定条件的元素索引
 %%%取旁瓣电平，即最大值之外的次大值
-FdB = FdB-3;
+FdB = FdB;
 for i = 1:2
     maxFdB = max(FdB);
     rr = find(FdB == maxFdB); %find()函数的功能是找到向量或者矩阵中不为0的元素，
