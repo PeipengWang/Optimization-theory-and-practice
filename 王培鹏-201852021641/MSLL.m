eb44f556a5e3c5ede41fcfa33b5b62fcec9fa6a6
PeipengWang <wangpeipeng123@163.com>
2019-06-09 21:36:12 +0800

    起名出了bug，修改一下

---
+++ 王培鹏-201852021641/MSLL.m
@@ -0,0 +1,46 @@
+%%%计算最大旁瓣%%%
+function  MSLL = MSLL(d,beta,NN,NL,seta0,f,L,maxE)
+seta = linspace(-pi,pi,NN);
+%alfa = (2*pi*(f(1:L/2)*(2.^(0:L/2-1)'))/(2^(L/2)-1)); %解码
+alfa = 0;
+%x = maxE/8*f(L/2+1:L)*(2.^(0:L/2-1)')/(2^(L/2)-1); %解码
+%I = [0.2035,0.2792,0.4089,0.5625,0.7147,0.8474,0.9466,1,1,0.9466,0.8474,...
+%   0.7147,0.5625,0.4089,0.2792,0.2035];
+for  i = 0:NL/2-1
+    I1(i+1) = (maxE*(f(i*L/16+1:(i+1)*L/16)*(2.^(0:(L/16-1))'))/(2^(L/16)-1));
+    %alfa1(i+1) = (2*pi*(f((i+8)*L/16+1:(i+9)*L/16)*(2.^(0:(L/16-1))'))/(2^(L/16)-1));
+end
+I2 = fliplr(I1);
+I = [I1,I2];
+for m = 1:NN
+    fai = beta*d*([0:NL-1]+1-(NL+1)/2)*cos(seta(m)-seta0)-[0:NL-1]*alfa;
+    Fit(m) = abs(sum(I*exp(sqrt(-1)*(fai)')));
+end
+%plot(alfa,fit)
+%hold on
+FdB = 20*log10(Fit/max(Fit));
+%那如果需要找到其中满足一定条件的元素索引
+%%%取旁瓣电平，即最大值之外的次大值
+for i = 1:2
+    maxFdB = max(FdB);
+    rr = find(FdB == maxFdB); %find()函数的功能是找到向量或者矩阵中不为0的元素，
+    mm = rr(1);
+    tu_up = 0;
+    while(FdB(mm+tu_up) >= FdB(mm+tu_up+1))
+        tu_up = tu_up+1;
+        if mm+tu_up+1>1800
+            mm = -tu_up+1;
+        end
+    end
+    mm = rr(1);
+    tu_down = 0;
+    while (FdB(mm-tu_down) >= FdB(mm-tu_down-1))
+        tu_down = tu_down+1;
+        if mm-tu_down-1<1
+            mm  = 1800+tu_down;
+        end
+    end
+    FdB(mm-tu_down:mm+tu_up) = -50;
+end
+MSLL = max(FdB);
+end
---
+++ 王培鹏-201852021641/main.m
@@ -0,0 +1,101 @@
+clear all;
+close all; 
+clc;
+NP=50; %个体数
+Pc = 0.8; %选择概率
+Pm = 0.05; %变异概率
+f=30000000;%信号频率
+c=30000000; 
+lamda = c/f;%波长
+d = lamda/2;%间距
+beta = 2*pi/lamda;%波数
+seta0 = 0;%波束方向
+G = 100;  %最大遗传代数
+L = 128; %编码数
+NL = 16; %阵元数
+NN = 1800; %划分刻度
+E0 = 1; %电压电流
+%%%生成初始种群
+f = randint(NP,L);  %产生一个L*NP的随机矩阵，正态分布
+%初始种群的分布
+maxE = 1;
+for i = 1:NP
+    for j = 1:L
+        if f(i,j)==1
+            plot(i,j)
+           hold on
+        end
+    end
+end
+%%%%遗传算法循环
+for k = 1:G
+    k
+    %解码，个体实际数 
+    % 计算峰值旁瓣比，即适应度
+    for i = 1:NP
+        F(i) = -MSLL(d,beta,NN,NL,seta0,f(i,:),L,maxE); % 取成正的，下面方便取最大值并且处理
+    end% 取得NP个个体的旁瓣电平
+    maxF = max(F)
+    minF = min(F)
+    rr = find(F == maxF);
+    fBest = f(rr(1),:);
+    Fin = (F-minF)/(maxF-minF); %归一化
+    %rr(1,1)
+    %fBest
+    %maxFit
+    %%%基于轮赌盘的选择操作
+    sum_Fin = sum(Fin);
+    fitvalue = Fin./sum_Fin;
+    fitvalue = cumsum(fitvalue);%cumsum(A) 返回一个和A同行同列的矩阵，
+                                %矩阵中第m行第n列元素是A中第1行到第m
+                                %行的所有第n列元素的累加和
+    ms = sort(rand(NP,1));
+    fiti = 1;
+    newi = 1;
+    while newi <= NP
+        if(ms(newi)<fitvalue(fiti))
+            nf(newi,:) = f(fiti,:);
+            newi = newi+1;
+        else
+            fiti = fiti+1;
+        end
+    end
+    %概率交叉
+    for i = 1:2:NP
+        P=rand;
+        if P<Pc
+            q = randint(1,L);%生成1*L随机矩阵，0或1
+            for j = 1:L
+                if q(j) == 1
+                    temp = nf(i+1,j);
+                    nf(i+1,j) = nf(i,j);
+                    nf(i,j) = temp;
+                end
+            end
+        end
+    end
+    %%%概率变异
+    for m = 1:NP
+        for n = 1:L
+            r = rand(1,1);
+            if r < Pm
+                nf(m,n) = ~nf(m,n);
+            end
+        end
+    end 
+    f = nf;
+    f(1,:) = fBest;
+    trace(k) = maxF;
+
+end
+figure
+plot(trace)
+xlabel('迭代次数')
+ylabel('目标函数值')
+title('适应度进化曲线')
+save fBestn.mat fBest
+save L.mat L
+save E.mat E0
+
+
+ 
